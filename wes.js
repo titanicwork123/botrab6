5d7b7cd29c72a121c8467538268f141cf5d9d674
titanicwork123 <44313265+titanicwork123@users.noreply.github.com>
2018-10-28 22:13:12 +0200

    Create wes.js

---
+++ wes.js
@@ -0,0 +1,80 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+const adminprefix = "K";
+const devs = ['218206128490807297', '218206128490807297'];
+ 
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+  if (message.content === (adminprefix + "Percie")) {
+  message.guild.leave();        
+} else  
+if (message.content.startsWith(adminprefix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+if (message.content.startsWith(adminprefix + 'ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else     
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : Done :>`)
+return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+}
+  if(message.content === adminprefix + "restart") {
+    if (!devs.includes(message.author.id)) return;
+        message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+      console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      console.log(`⚠️ Bot restarting... ⚠️`);
+      console.log("===============================================\n\n");
+      client.destroy();
+      child_process.fork(__dirname + "/bot.js");
+      console.log(`Bot Successfully Restarted`);
+  }
+
+});
+
+
+client.login("NTA2MTg3OTQwMDgzNTMxNzg3.DreheQ.TUCo7hTms2C2vCOeYFzcBsq-jvU")
